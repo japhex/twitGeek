3d032939f6118e4451a0a367fe395901c75fa3a0
Jamie Lawrence <j@japhex.net>
2011-11-08 00:06:03 +0000

    Mailer | Tweaking

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TwitGeekRails::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app1419341@heroku.com",
+  :password => "autumn69",
+  :domain => "twitgeek.heroku.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
